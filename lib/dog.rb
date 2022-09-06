7f707918a048e2bf09725ee3617d9788c6c363e0
Torrey Kanko <torphus101@gmail.com>
2022-09-06 11:33:14 -0700

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Dog
+    def name=(dogs_name)
+        @the_dogs_name = dogs_name
+    end
+    def name
+        @the_dogs_name
+    end
+    def bark
+        puts "woof!"
+    end
+end
